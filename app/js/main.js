3334c66ee6c7f3127253566d7071a4634449cd55
shadkonstantin <shadkonstantin@yandex.ru>
2020-09-14 19:54:08 +0500

    fixed accordeon, fonts, callback

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,18 @@
-$(function(){
+$(function () {
 
-    $(window).scroll(function(){
-        $('.header__top').toggleClass('sticky', $(this).scrollTop() > 0);
-        $('.header__menu-link').toggleClass('font-dark', $(this).scrollTop() > 0);
-        $('.header__info-link').toggleClass('font-dark', $(this).scrollTop() > 0);
+    $(window).scroll(function () {
+        $(".header__top").toggleClass("header__top--scroll", $(this).scrollTop() > 0);
     });
 
-    // $(".arrow-down").on("click", function () {
-    //     $(".faq__answer1").slideToggle();
-    //     $(this).toggleClass('rotate180');
-    // });
 
-    var title = document.getElementsByClassName('arrow-down'),
+    var item = document.getElementsByClassName('faq__item'),
         content = document.getElementsByClassName('faq__answer');
 
-    for (var i=0; i < title.length; i++){
-        title[i].addEventListener('click', function(){
-            if(!(this.classList.contains('active'))){
-                for(var i = 0; i<title.length; i++){
-                    title[i].classList.remove('active');
+    for (var i = 0; i < item.length; i++) {
+        item[i].addEventListener('click', function () {
+            if (!(this.classList.contains('active'))) {
+                for (var i = 0; i < item.length; i++) {
+                    item[i].classList.remove('active');
                 }
                 this.classList.add('active');
             }
