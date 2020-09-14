831c52c2f2d30ebd23664736eded286e1ece3bc7
Konstantin <shadkonstantin@yandex.ru>
2020-09-14 15:14:30 +0500

    section FAQ not finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function(){
 
+    $(window).scroll(function(){
+        $('.header__top').toggleClass('sticky', $(this).scrollTop() > 0);
+        $('.header__menu-link').toggleClass('font-dark', $(this).scrollTop() > 0);
+        $('.header__info-link').toggleClass('font-dark', $(this).scrollTop() > 0);
+    });
 
-
-  
+    $(".arrow-down").on("click", function () {
+        $(".faq__answer").slideToggle();
+        $(this).toggleClass('rotate180');
+    });
+   
 });
