f7f10f88d43fbc1e6c4ff340dcd7aab0e78f8d13
Konstantin <shadkonstantin@yandex.ru>
2020-09-14 16:21:01 +0500

    try fix accordeon

--- app/js/main.js
+++ app/js/main.js
@@ -6,9 +6,23 @@ $(function(){
         $('.header__info-link').toggleClass('font-dark', $(this).scrollTop() > 0);
     });
 
-    $(".arrow-down").on("click", function () {
-        $(".faq__answer").slideToggle();
-        $(this).toggleClass('rotate180');
-    });
-   
+    // $(".arrow-down").on("click", function () {
+    //     $(".faq__answer1").slideToggle();
+    //     $(this).toggleClass('rotate180');
+    // });
+
+    var title = document.getElementsByClassName('arrow-down'),
+        content = document.getElementsByClassName('faq__answer');
+
+    for (var i=0; i < title.length; i++){
+        title[i].addEventListener('click', function(){
+            if(!(this.classList.contains('active'))){
+                for(var i = 0; i<title.length; i++){
+                    title[i].classList.remove('active');
+                }
+                this.classList.add('active');
+            }
+        })
+    }
+
 });
