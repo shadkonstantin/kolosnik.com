806fcab4027dbd911a157caf1fecec1ca403486e
Konstantin <shadkonstantin@yandex.ru>
2020-09-17 16:15:09 +0500

    finished production page

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,81 @@
 $(function () {
 
 
-    $(window).scroll(function () {
-        $(".header__top").toggleClass("header__top--scroll", $(this).scrollTop() > 0);
-    });
-
-
-    var item = document.getElementsByClassName('faq__item'),
-        content = document.getElementsByClassName('faq__answer');
-
-    for (var i = 0; i < item.length; i++) {
-        item[i].addEventListener('click', function () {
-            if (!(this.classList.contains('active'))) {
-                for (var i = 0; i < item.length; i++) {
-                    item[i].classList.remove('active');
-                }
-                this.classList.add('active');
+        $(window).scroll(function () {
+                $(".header__top").toggleClass("header__top--scroll", $(this).scrollTop() > 0);
             }
-        })
-    };
 
-    $(".header__menu-btn").on("click", function () {
-        $(".header__menu ul").slideToggle();
-    });
+        );
 
 
-    $(".header__info-icon").on("click", function () {
-        $(".header__top .header__info").slideToggle();
-    });
+        var item = document.getElementsByClassName('faq__item'),
+            content = document.getElementsByClassName('faq__answer');
 
-    $(".option-list").on("click", function () {
-        $(".goods__wrapper").removeClass('grid');
-        $(this).addClass('active');
-        $('.option-grid').removeClass('active');
+        for (var i = 0; i < item.length; i++) {
+            item[i].addEventListener('click', function () {
+                    if (!(this.classList.contains('active'))) {
+                        for (var i = 0; i < item.length; i++) {
+                            item[i].classList.remove('active');
+                        }
 
-    });
-    $(".option-grid").on("click", function () {
-        $(".goods__wrapper").addClass('grid');
-        $(this).addClass('active');
-        $('.option-list').removeClass('active');
-    });
+                        this.classList.add('active');
+                    }
+                }
 
-    $('select').styler();
+            )
+        }
 
-});
+        ;
+
+        $(".header__menu-btn").on("click", function () {
+                $(".header__menu ul").slideToggle();
+            }
+
+        );
+
+
+        $(".header__info-icon").on("click", function () {
+                $(".header__top .header__info").slideToggle();
+            }
+
+        );
+
+        $(".option-list").on("click", function () {
+                $(".goods__wrapper").removeClass('grid');
+                $(this).addClass('active');
+                $('.option-grid').removeClass('active');
+
+            }
+
+        );
+
+        $(".option-grid").on("click", function () {
+                $(".goods__wrapper").addClass('grid');
+                $(this).addClass('active');
+                $('.option-list').removeClass('active');
+            }
+
+        );
+
+        $(".option-list").on("click", function () {
+                $(".information__wrapper").addClass('list');
+                $(this).addClass('active');
+                $('.option-grid').removeClass('active');
+
+            }
+
+        );
+
+        $(".option-grid").on("click", function () {
+                $(".information__wrapper").removeClass('list');
+                $(this).addClass('active');
+                $('.option-list').removeClass('active');
+            }
+
+        );
+
+        $('select').styler();
+
+    }
+
+);
