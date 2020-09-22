7d243969cc7986ac43101cb4e28a2196e45a5a72
Konstantin <shadkonstantin@yandex.ru>
2020-09-22 11:46:38 +0500

    fix JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,174 +1,118 @@
 $(function () {
+  $(window).scroll(function () {
+    $(".header__top").toggleClass(
+      "header__top--scroll",
+      $(this).scrollTop() > 0
+    );
+  });
 
+  var item = document.getElementsByClassName("faq__item"),
+    content = document.getElementsByClassName("faq__answer");
 
-        $(window).scroll(function () {
-                $(".header__top").toggleClass("header__top--scroll", $(this).scrollTop() > 0);
-            }
-
-        );
-
-
-        var item = document.getElementsByClassName('faq__item'),
-            content = document.getElementsByClassName('faq__answer');
-
+  for (var i = 0; i < item.length; i++) {
+    item[i].addEventListener("click", function () {
+      if (!this.classList.contains("active")) {
         for (var i = 0; i < item.length; i++) {
-            item[i].addEventListener('click', function () {
-                    if (!(this.classList.contains('active'))) {
-                        for (var i = 0; i < item.length; i++) {
-                            item[i].classList.remove('active');
-                        }
-
-                        this.classList.add('active');
-                    }
-                }
-
-            )
+          item[i].classList.remove("active");
         }
 
-        ;
+        this.classList.add("active");
+      }
+    });
+  }
 
-        $(".header__menu-btn").on("click", function () {
-                $(".header__menu ul").slideToggle();
-            }
+  $(".header__menu-btn").on("click", function () {
+    $(".header__menu ul").slideToggle();
+  });
 
-        );
+  $(".header__info-icon").on("click", function () {
+    $(".header__top .header__info").slideToggle();
+  });
 
+  $(".option-list").on("click", function () {
+    $(".goods__wrapper").removeClass("grid");
+    $(this).addClass("active");
+    $(".option-grid").removeClass("active");
+  });
 
-        $(".header__info-icon").on("click", function () {
-                $(".header__top .header__info").slideToggle();
-            }
+  $(".option-grid").on("click", function () {
+    $(".goods__wrapper").addClass("grid");
+    $(this).addClass("active");
+    $(".option-list").removeClass("active");
+  });
 
-        );
+  $(".option-list").on("click", function () {
+    $(".information__wrapper").addClass("list");
+    $(this).addClass("active");
+    $(".option-grid").removeClass("active");
+  });
 
-        $(".option-list").on("click", function () {
-                $(".goods__wrapper").removeClass('grid');
-                $(this).addClass('active');
-                $('.option-grid').removeClass('active');
+  $(".option-grid").on("click", function () {
+    $(".information__wrapper").removeClass("list");
+    $(this).addClass("active");
+    $(".option-list").removeClass("active");
+  });
 
-            }
+  $('select[id="product"]').styler();
+  $('input[id="popup__checkbox"]').styler();
+  $("select").styler();
 
-        );
+  $(".header__button-link").magnificPopup({
+    type: "inline",
+  });
 
-        $(".option-grid").on("click", function () {
-                $(".goods__wrapper").addClass('grid');
-                $(this).addClass('active');
-                $('.option-list').removeClass('active');
-            }
+  $(".goods__item-btn").magnificPopup({
+    type: "inline",
+  });
 
-        );
+  const polosaText = document.getElementById("polosa_text");
+  const cellText = document.getElementById("cell_text");
+  const prutokText = document.getElementById("prutok_text");
+  const checkbox = document.getElementById("checkbox_text");
+  const title = document.getElementById("goods__title-count");
+  const length_text = document.getElementById("length_text");
+  const width_text = document.getElementById("width_text");
 
-        $(".option-list").on("click", function () {
-                $(".information__wrapper").addClass('list');
-                $(this).addClass('active');
-                $('.option-grid').removeClass('active');
+  const buttonsProduct = document.querySelectorAll(".goods__item-btn");
 
-            }
+  for (let btn of buttonsProduct) {
+    btn.addEventListener("click", () => {
+      takeActiveProduct(btn);
+    });
+  }
 
-        );
+  const takeActiveProduct = (currentActive) => {
+    title.innerHTML = currentActive.parentElement.parentElement.querySelector(
+      ".goods__item-title"
+    ).textContent;
 
-        $(".option-grid").on("click", function () {
-                $(".information__wrapper").removeClass('list');
-                $(this).addClass('active');
-                $('.option-list').removeClass('active');
-            }
+    polosaText.innerHTML = currentActive.parentElement.parentElement.querySelector(
+      ".polosa"
+    ).textContent;
+    cellText.innerHTML = currentActive.parentElement.parentElement.querySelector(
+      ".cell"
+    ).textContent;
+    prutokText.innerHTML = currentActive.parentElement.parentElement.querySelector(
+      ".prutok"
+    ).textContent;
 
-        );
-
-        $('select[class="stupen__select"]').styler();
-        $('select[id="product"]').styler();
-        $('input[id="popup__checkbox"]').styler();
-
-
-
-
-        $('.header__button-link').magnificPopup({
-            type: "inline"
-        });
-
-        $('.goods__item-btn').magnificPopup({
-            type: "inline"
-        });
-
-        $("#goods__item-btn1").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count1').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count1').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count1').textContent;
-            if (document.querySelector('#checkbox1').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-
-        });
-        $("#goods__item-btn2").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count2').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count2').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count2').textContent;
-            if (document.querySelector('#checkbox2').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-        });
-        $("#goods__item-btn3").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count3').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count3').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count3').textContent;
-            if (document.querySelector('#checkbox3').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-        });
-        $("#goods__item-btn4").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count4').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count4').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count4').textContent;
-            if (document.querySelector('#checkbox4').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-        });
-        $("#goods__item-btn5").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count5').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count5').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count5').textContent;
-            if (document.querySelector('#checkbox5').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-        });
-        $("#goods__item-btn6").on("click", function () {
-            document.querySelector('#goods__title-count').innerHTML = document.querySelector('.goods__item-title').textContent;
-            document.querySelector('#polosa_text').innerHTML = document.querySelector('#polosa-count6').textContent;
-            document.querySelector('#cell_text').innerHTML = document.querySelector('#cell-count6').textContent;
-            document.querySelector('#prutok_text').innerHTML = document.querySelector('#prutok-count6').textContent;
-            if (document.querySelector('#checkbox6').checked) {
-                document.querySelector('#checkbox_text').innerHTML = "да"
-
-            } else {
-                document.querySelector('#checkbox_text').innerHTML = "нет"
-
-            }
-        });
-
+    if (
+      currentActive.parentElement.parentElement.querySelector(
+        ".checkbox__product"
+      ).checked
+    ) {
+      checkbox.innerHTML = "да";
+    } else {
+      checkbox.innerHTML = "нет";
     }
+    const length = currentActive.parentElement.parentElement.querySelector(
+      ".stupen__select-length"
+    ).firstChild.value;
+    const width = currentActive.parentElement.parentElement.querySelector(
+      ".stupen__select-width"
+    ).firstChild.value;
 
-);
+    length_text.innerHTML = length;
+    width_text.innerHTML = width;
+  };
+});
