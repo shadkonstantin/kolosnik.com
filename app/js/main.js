b377f55fd910ea818097f617340f899c86a60530
shadkonstantin <shadkonstantin@yandex.ru>
2020-09-23 23:20:11 +0500

    gallery

--- app/js/main.js
+++ app/js/main.js
@@ -205,3 +205,21 @@ $(document).ready(function () {
     checkWidth(); // проверит при изменении размера окна клиента
   });
 });
+
+// This will create a single gallery from all elements that have class "gallery-item"
+$('.gallery-item').magnificPopup({
+  type: 'image',
+  gallery: {
+    enabled: true, // set to true to enable gallery
+
+    preload: [0, 2], // read about this option in next Lazy-loading section
+
+    navigateByImgClick: true,
+
+    arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+    tPrev: 'Previous (Left arrow key)', // title for left button
+    tNext: 'Next (Right arrow key)', // title for right button
+    tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+  }
+});
